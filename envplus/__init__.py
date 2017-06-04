caa3dc0cbe9ca85b58df74045a43c5bc83559396
Tom Picton <tom@tompicton.co.uk>
2017-06-04 12:44:41 +0100

    Fix module imports under Python 3

--- envplus/__init__.py
+++ envplus/__init__.py
@@ -1,6 +1,8 @@
+from __future__ import absolute_import
+
 import env
-import pathfile
-import helpers
+import envplus.pathfile
+import envplus.helpers
 
 VERSION_TUPLE = (0, 0, 0)
 VERSION = ".".join(map(str, VERSION_TUPLE))
