aaa42f6a6f377b94cdb25075c3c3104461b9519d
Payson Wallach <paysonwallach@icloud.com>
2019-12-05 23:06:38 -0600

    Add base class for exceptions

---
+++ venn/exceptions.py
@@ -0,0 +1,14 @@
+#
+# Venn
+#
+# Copyright (c) 2019 Payson Wallach
+#
+# Released under the terms of the Hippocratic License
+# (https://firstdonoharm.dev/version/1/1/license.html)
+#
+
+
+class VennException(Exception):
+    """Base exception class."""
+
+    pass
